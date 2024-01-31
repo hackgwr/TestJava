7161de6124f3678eb67231425a2fefbdbf70984f
hackgwr <hackgwr@gmail.com>
2024-01-31 14:04:12 +0900

    java test

--- list/src/com/itbaizhan/MapTest.java
+++ list/src/com/itbaizhan/MapTest.java
@@ -10,7 +10,8 @@ public class MapTest {
 //        test01();
 //        test02();
 //        iterateList();
-        iterateSet();
+//        iterateSet();
+        iterateMap();
 
     }
 
@@ -18,14 +19,12 @@ public class MapTest {
     //Map的常用方法
     public static void test01(){
         Map m1 = new HashMap();//new HashTable
-
         //常用方法put
         m1.put(1,"one");//map中存储的是对象，这里的1不是int而是Integer.valueof() ，如下
 //        m1.put(Integer.valueOf(1),"one");
         m1.put(2,"two");
         m1.put(3,"three");
         m1.put(4,"three");//键不可重复，值可以重复
-
         m1.put(2,"二");
         //测试直接再在2键值中加入value，输出结果为新添加的value直接覆盖了之前的value
 
@@ -38,19 +37,16 @@ public class MapTest {
         System.out.println(m1.isEmpty());//判断是否为空
         System.out.println(m1.containsKey(1));//判断m1中的键是否包含1这个键
         System.out.println(m1.containsValue("one"));//判断m1中的值是否包含one这个值
-
         m1.remove(3);//从Map容器中移除键值对
         System.out.println(m1);
 
         Map m2 = new HashMap();
 //        Map m2 = new HashMap<>();
 //        java6以下需要加入<>并添加键和值的类型，java7以上则不需要，<>可以省略
-
         m2.put("yi","1111");
         m2.put("er","2222");
         m2.put(3,"3333");
         System.out.println(m2);
-
         m1.putAll(m2);//将Map m2的全部键值对放入Map m1中
         System.out.println(m1);
     }
@@ -89,6 +85,11 @@ public class MapTest {
         //使用迭代器循环遍历List
         System.out.println("使用迭代器循环遍历List");
         Iterator<String> iterator = list01.iterator();
+        //使用增强型for循环遍历
+        for(String i:list01){
+            System.out.println(i);
+        }
+        //使用while循环遍历
         while (iterator.hasNext()) {
             String a = iterator.next();
             System.out.println(a);
@@ -104,8 +105,9 @@ public class MapTest {
         set01.add("rui");
         System.out.println(set01);
 
-        //使用for循环遍历Set
-        System.out.println("使用for循环遍历Set");
+        //由于Set没有索引，所以我们不能使用for循环遍历Set，但是可以用增强for循环或迭代器的方法遍历set
+        //可以将set的内容放入数组再进行for循环遍历
+        System.out.println("使用for循环遍历Set生成的数组");
         for (int i=0;i<set01.size();i++){
             Object[] object = set01.toArray();
             System.out.println(object[i]);
@@ -120,15 +122,65 @@ public class MapTest {
         //使用迭代器循环遍历Set
         System.out.println("使用迭代器循环遍历Set");
         Iterator<String> iterator = set01.iterator();
+        //使用增强型for循环遍历
+        System.out.println("使用增强型for循环遍历");
+        for(String b:set01){
+            System.out.println(b);
+        }
+        //使用while循环遍历
+        System.out.println("使用while循环遍历");
         while (iterator.hasNext()) {
             String a = iterator.next();
             System.out.println(a);
         }
-
     }
 
+    //容器Map的遍历
+    public static void iterateMap(){
+        Map<String,String> map01 = new HashMap<>();
+        map01.put("one","yi");
+        map01.put("two","er");
+        map01.put("three","san");
+        Set<String> mp1= map01.keySet();
+        System.out.println(mp1);//将Map中的元素放入新建立的Set中，之后可以用增强for循环或者迭代器进行遍历
 
+        //使用增强型for循环遍历
+        System.out.println("使用增强型for循环遍历Set");
+        for (String i:mp1){
+            System.out.println(i);//获取键
+            System.out.println(map01.get(i));//获取值
+        }
 
+        //使用迭代器循环遍历
+        System.out.println("使用迭代器循环遍历Set");
+        Iterator<String> iterator = mp1.iterator();
+        //使用增强型for循环遍历
+        System.out.println("使用增强型for循环遍历");
+        for(String b:mp1){
+            System.out.println(b);//获取键
+            System.out.println(map01.get(b));//获取值
+        }
+        //使用while循环遍历
+        System.out.println("使用while循环遍历");
+        while (iterator.hasNext()) {
+            String a = iterator.next();
+            System.out.println(a);//获取键
+            System.out.println(map01.get(a));//获取值
+        }
+
+        Collection<String> coll = map01.values();
+        System.out.println("使用增强型for循环遍历Set");
+        for (String i:coll){
+            System.out.println(i);//获取值
+        }
+
+        //同时获取键值,利用EntrySet这个特殊类型遍历键值对
+        Set<Map.Entry<String,String>> entrySet = map01.entrySet();
+        System.out.println("使用增强型for循环遍历Set");
+        for (Map.Entry i:entrySet){
+            System.out.println(i);//获取值
+        }
+    }
 }
 
 
